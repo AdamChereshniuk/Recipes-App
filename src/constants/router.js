216dce248e1cb548a9ebb0b8e2172e7a59af00b5
Adam <cheradam2011@yandex.ru>
2026-09-21 15:37:33 +0700

    4 commit

--- src/constants/router.js
+++ src/constants/router.js
@@ -1,9 +1,9 @@
 export const ROUTES_PATHS = {
-    HOME: "/",
-    REGISTER: "/register",
-    LOGIN: "/login",
-    PROFILE: "/profile",
-    RECIPE_VIEW: "/recipes/view/:id",
-    RECIPE: "/recipes/:id",
-    RESET_PASSWORD: "/#",
+    HOME: "/Recipes-App/",
+    REGISTER: "/Recipes-App/register",
+    LOGIN: "/Recipes-App/login",
+    PROFILE: "/Recipes-App/profile",
+    RECIPE_VIEW: "/Recipes-App/recipes/view/:id",
+    RECIPE: "/Recipes-App/recipes/:id",
+    RESET_PASSWORD: "/Recipes-App/#",
 };
