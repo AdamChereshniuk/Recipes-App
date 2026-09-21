5b4a5d18fa3506553f32a6f2c3897274b8c101fe
Adam <cheradam2011@yandex.ru>
2026-09-21 15:49:56 +0700

    n commit

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://lvtuznbuqmiskxntidyw.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2dHV6bmJ1cW1pc2t4bnRpZHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1MjgwNTYsImV4cCI6MjEwNDEwNDA1Nn0.gMeA90lbY8c9LzkFnhYMOGEHswqacQPLyYyrx-TovLs";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
